f6e9babb74feee2358aa2dcc823bf9906430bc7b
raygoon <jasonraycat@gmail.com>
2022-07-05 11:50:55 +0900

    m

--- rooms/admin.py
+++ rooms/admin.py
@@ -7,6 +7,11 @@ class ItemAdmin(admin.ModelAdmin):
 
     """Item Admin Definition"""
 
+    list_display = ("name", "used_by")
+
+    def used_by(self, obj):
+        return obj.rooms.count()
+
     pass
 
 
@@ -14,6 +19,29 @@ class ItemAdmin(admin.ModelAdmin):
 class RoomAdmin(admin.ModelAdmin):
 
     """Room Admin Definition"""
+
+    fieldsets = (
+        (
+            "Basic Info",
+            {"fields": ("name", "description", "country", "address", "price")},
+        ),
+        (
+            "Times",
+            {"fields": ("check_in", "check_out", "instant_book")},
+        ),
+        (
+            "Spaces",
+            {"fields": ("guests", "beds", "bedrooms", "baths")},
+        ),
+        (
+            "More About the Spaces",
+            {"fields": ("amenities", "facilities", "house_rules")},
+        ),
+        (
+            "Last Details",
+            {"fields": ("host",)},
+        ),
+    )
 
     list_display = (
         "name",
@@ -27,15 +55,33 @@ class RoomAdmin(admin.ModelAdmin):
         "check_in",
         "check_out",
         "instant_book",
+        "count_amenities",
+        "count_photos",
+        "total_rating",
     )
+
+    ordering = ("name", "price", "bedrooms")
 
     list_filter = (
         "instant_book",
+        "host__superhost",
         "country",
         "city",
+        "room_type",
+        "amenities",
+        "facilities",
+        "house_rules",
     )
 
-    search_fields = ("=city", "^host__username")
+    search_fields = ("^city", "^host__username")
+
+    filter_horizontal = ("amenities", "facilities", "house_rules")
+
+    def count_amenities(self, obj):
+        return obj.amenities.count()
+
+    def count_photos(self, obj):
+        return obj.photos.count()
 
 
 @admin.register(models.Photo)
--- rooms/models.py
+++ rooms/models.py
@@ -60,7 +60,7 @@ class Photo(core_models.TimeStampedModel):
 
     # 파이썬 코드는 수직해석이라서 Room 클래스의 뒤쪽에 있어야 하지만,
     # 클래스 코드의 위치를 변경하지 않고 아래처럼 클래스네임을 string으로 처리하는 방법을 사용할 수 있다. ("Room")
-    room = models.ForeignKey("Room", on_delete=models.CASCADE)
+    room = models.ForeignKey("Room", related_name="photos", on_delete=models.CASCADE)
 
     def __str__(self):
         return self.caption
@@ -87,14 +87,25 @@ class Room(core_models.TimeStampedModel):
     # 호스트는 유저이기도 하다 이처럼 특정데이터베이스와 다른 데이터베이스를 연결하려 할때는
     # Foreignkey 를 사용해서 연결해준다.
     # on_delete=models.CASCADE 는 User에 종속된 모든 데이터베이스 필드의 정보들은 User 가 삭제되면 같이 삭제된다는 뜻이다.
-    host = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    host = models.ForeignKey(
+        "users.User", related_name="rooms", on_delete=models.CASCADE
+    )
 
     # Foreinkey 는 Field가 1대다 의 관계일때 ManyToManyField 는 다대다의 관계일때 사용한다.
-    room_type = models.ForeignKey("RoomType", on_delete=models.SET_NULL, null=True)
-    amenities = models.ManyToManyField("Amenity", blank=True)
-    facility = models.ManyToManyField("Facility", blank=True)
-    houseRule = models.ManyToManyField("HouseRule", blank=True)
+    room_type = models.ForeignKey(
+        "RoomType", related_name="rooms", on_delete=models.SET_NULL, null=True
+    )
+    amenities = models.ManyToManyField("Amenity", related_name="rooms", blank=True)
+    facilities = models.ManyToManyField("Facility", related_name="rooms", blank=True)
+    house_rules = models.ManyToManyField("HouseRule", related_name="rooms", blank=True)
 
     # Rooms model row 위쪽에 나오는 Rooms Object ID 대신 user 이름이 나오도록 __str__을 사용해서 아래와 같이 세팅해 줄수 있다.
     def __str__(self):
         return self.name
+
+    def total_rating(self):
+        all_reviews = self.reviews.all()
+        all_ratings = 0
+        for review in all_reviews:
+            all_ratings += review.rating_average()
+        return all_ratings / len(all_reviews)
